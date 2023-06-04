1dfe2acfb58d42410a2b5c1afef0d1bf1043d1b0
junka <wan.junjie@foxmail.com>
2023-06-05 00:04:05 +0800

    fix classifiers and bump version 0.5.2
    Signed-off-by: junka <wan.junjie@foxmail.com>

--- pktperf/__init__.py
+++ pktperf/__init__.py
@@ -2,7 +2,7 @@
 pktperf provides pktgen helper scripts functions for pps
 performance test.
 """
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 __author__ = "junka"
 __maintainer__ = "junka"
 __license__ = "BSD"
